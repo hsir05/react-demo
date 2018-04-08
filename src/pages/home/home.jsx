3cea28d20ad0ccce45e2e0459b7a38e80174526e
hdq <haqsir@163.com>
2018-04-08 15:30:46 +0800

    添加antd

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
-import './home.scss';
+import React, { Component } from 'react'
+import './home.scss'
+import { Button } from 'antd'
+
 
 export default class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: false,
+      iconLoading: false,
+    }
+  }
+
+  enterLoading = () => {
+    this.setState({ loading: true });
+  }
+
+  enterIconLoading = () => {
+    this.setState({ iconLoading: true });
+  }
+
+
+
   render() {
     return (
       <div className="home">
-      home
+        <Button type="primary" size="small" loading>
+           Loading
+         </Button>
       </div>
     )
   }
