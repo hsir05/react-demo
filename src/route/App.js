bc04240e2b28c40640f823e1801bd2bafbf6921d
hdq <haqsir@163.com>
2018-04-11 10:42:18 +0800

    登录后台连接成功

--- src/route/App.js
+++ src/route/App.js
@@ -6,7 +6,6 @@ import {
 
 import login from '../pages/login/login'
 import home from '../pages/home/home'
-import activity from '../pages/activity/activity'
 
 import './App.css';
 
@@ -17,8 +16,6 @@ export default class App extends Component {
         <Switch>
           <Route exact path='/' component={home}/>
           <Route path='/login' component={login}/>
-          <Route path='/activity' component={activity}/>
-
         </Switch>
      </Router>
     )
